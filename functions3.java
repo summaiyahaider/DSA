a107b24cd8194f000a686a758253942263ceefcd
Summaiya Haider <summaiya@student.iul.ac.in>
2022-10-24 22:24:49 +0530

    init

--- functions3.java
+++ functions3.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 public class functions3 {
-    public static int multiplyInt(int a, int b){
-     int product=a*b;
-     return product;
+    public static int multiplyInt(int a, int b) {
+        int product = a * b;
+        return product;
     }
+public static void main(String args[]){
+    System.out.println("Enter two integers");
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    int product = multiplyInt(a, b);
+
+    System.out.println("The product of the two integers is: "+product);
+
+}
 }
