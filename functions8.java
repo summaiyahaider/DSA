5437ce7fa8e60ae467d090f4e8d16421a52fc242
Summaiya Haider <summaiya@student.iul.ac.in>
2022-10-30 23:39:52 +0530

    fibonacci series

--- functions8.java
+++ functions8.java
@@ -1,7 +1,29 @@
 import java.util.*;
 
 public class functions8 {
-    public static int greatestCommomFactor(int a, int b){
+    public static void fibonacciNumbers(int n){
+
+        int a = 0;
+        int b = 1;
+
+        for(int i=2; i<=n; i++){
+
+            System.out.print(a+" ");
+
+            int temp = b;
+            b = a+b;
+            a=temp;
+
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
         
+        fibonacciNumbers(x);
     }
 }
