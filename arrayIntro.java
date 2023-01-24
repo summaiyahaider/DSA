300f5ca7f0ee1a0308d19dee15a9f66b377f3aea
Summaiya Haider <summaiya@student.iul.ac.in>
2023-01-24 22:42:00 +0530

    Array Intro

---
+++ arrayIntro.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class arrayIntro {
+    public static void main(String args[]){
+        System.out.println("Enter an integer");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int arr[]=new int[size];
+        System.out.println("Enter the numbers in the array");
+        for(int i=0; i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < arr.length; i++ ){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
---
+++ arrayIntro2.java
@@ -0,0 +1,24 @@
+//Take an array of names as input from the user and print them on the screen
+import java.util.*;
+import java.lang.*;
+public class arrayIntro2 {
+    
+        public static void main(String args[]) {
+           Scanner sc = new Scanner(System.in);
+           int size = sc.nextInt();
+           String names[] = new String[size];
+     
+     
+           //input
+           for(int i=0; i<size; i++) {
+               names[i] = sc.next();
+           }
+          
+           //output
+            for(int i=0; i<names.length; i++) {
+                System.out.println("name " + (i+1) +" is : " + names[i]);
+            }
+           
+        }
+     
+    }
