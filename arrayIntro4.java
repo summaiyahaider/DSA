81081376685d79bc551c565a08bed8bcfe2e0bda
Summaiya Haider <summaiya@student.iul.ac.in>
2023-01-25 23:48:14 +0530

    init

---
+++ arrayIntro4.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class arrayIntro4 {
+    public static void main(String args[]){
+        System.out.println("Enter the number of names");
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
