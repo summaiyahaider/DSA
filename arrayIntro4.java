22d94f6ec8d5c0ae114603f3aa14301683281cfa
Summaiya Haider <summaiya@student.iul.ac.in>
2023-02-02 13:40:18 +0530

    min max

--- arrayIntro4.java
+++ arrayIntro4.java
@@ -1,7 +1,10 @@
+// Finding maximum and minimum number in an array of integers  
+
 import java.util.*;
 public class arrayIntro4 {
     public static void main(String args[]){
-        System.out.println("Enter the number of names");
+
+        System.out.println("Enter the size of integers");
 
         
 
@@ -9,6 +12,31 @@ public class arrayIntro4 {
 
         int size = sc.nextInt();
 
+        System.out.println("Enter the integers in the array");
+
         int arr[] = new int[size];
+
+        for(int i = 0; i <size; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // Read about max and min
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i <arr.length; i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("This is the minimum value in the array: " + min);
+        System.out.println("This is the maximum value in the array: " + max);
+
+        sc.close();
     }
 }
